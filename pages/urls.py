5816bfaccb8818977543ca43ce949a445e40f7bc
miguelpena-bigdata-dev <miguel674@revature.net>
2025-02-18 12:44:26 -0700

    learned how to test endpoints, and make dynamic links

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home_page_view, about_page_view
+from .views import home_page_view, AboutPageView
 
 urlpatterns = [
-    path("about/", about_page_view, name="about"),
+    path("about/", AboutPageView.as_view(), name="about"),
     path("", home_page_view, name="home")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
-def home_page_view(request):
-    return HttpResponse("Homepage")
-
-def about_page_view(request):
+def home_page_view(request: HttpRequest) -> HttpResponse:
     context = {
-        "name": "Miguel",
-        "age": 32
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "Thank you for visiting."
     }
+    return render(request, "home.html", context)
+
+# def about_page_view(request: HttpRequest) -> HttpResponse:
+#     context = {
+#         "name": "Miguel",
+#         "age": 32
+#     }
     
-    return render(request, "pages/about.html", context)
+#     return render(request, "pages/about.html", context)
+
+class AboutPageView(TemplateView): 
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-555-5555"
+        return context
