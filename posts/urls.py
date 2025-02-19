ffca61e17ac8ed6505c7bfb9cef377239ba4004f
miguelpena-bigdata-dev <miguel674@revature.net>
2025-02-19 11:25:40 -0700

    dynamic link and listing of posts

--- posts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PostList
-
-urlpatterns = [
-    path("posts/", PostList.as_view(), name="post_list")
-]
--- posts/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import ListView
-from django.http import HttpRequest, HttpResponse
-from .models import Post
-
-class PostList(ListView):
-    model = Post
-    template_name = "post_list.html"
-
