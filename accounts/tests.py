93758c255d0ca1e451b220d07f24734941d1e82a
miguelpena-bigdata-dev <miguel674@revature.net>
2025-03-05 10:44:38 -0700

    adjusted templates to better match source

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse  # new
+
 
 class UsersManagersTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
-            username="testuser", 
-            email="testuser@example.com", 
+            username="testuser",
+            email="testuser@example.com",
             password="testpass1234",
         )
         self.assertEqual(user.username, "testuser")
@@ -28,6 +29,7 @@ class UsersManagersTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
 
 class SignupPageTests(TestCase):  # new
     def test_url_exists_at_correct_location_signupview(self):
@@ -52,6 +54,4 @@ class SignupPageTests(TestCase):  # new
         self.assertEqual(response.status_code, 302)
         self.assertEqual(get_user_model().objects.all().count(), 1)
         self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
-        self.assertEqual(
-          get_user_model().objects.all()[0].email, "testuser@email.com"
-        )
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@email.com")
