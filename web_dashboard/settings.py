5f886530e242fe0dfafd1355a7a9994083798cf6
miguelpena-bigdata-dev <miguel674@revature.net>
2025-03-04 18:24:24 -0700

    password change and reset

--- web_dashboard/settings.py
+++ web_dashboard/settings.py
@@ -137,3 +137,5 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
