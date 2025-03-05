43b5f93af6ad1d2a4069adb6fd7c99d29fd88efc
miguelpena-bigdata-dev <miguel674@revature.net>
2025-03-04 17:23:46 -0700

    user authentication added

--- web_dashboard/urls.py
+++ web_dashboard/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
-    path("", include("blog.urls"))
+    path("", TemplateView.as_view(template_name="home.html"), name="home")
+    # path("", include("blog.urls"))
     # path("", include("pages.urls"))
     # path("", include("posts.urls")),
 ]
